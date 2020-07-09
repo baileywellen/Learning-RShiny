eff67b848f934b31cd7d272d8ba70cd58b6e5dbb
baileywellen <baileywellen@gmail.com>
2020-07-09 15:10:18 -0500

    Allow User Choice of Details to Select
    Added a choice to view pie chart of mental illness, age, armed, race, etc. Put ages into aggregate "bins". Moved sidebar to right hand side and split it between the two selection panes.

--- police_shootings.R
+++ police_shootings.R
@@ -1,13 +1,43 @@
 #The below 4 lines are the "Template" For a Shiny App 
 #import the library
 library(shiny)
+
+possible_graphs <- c("gender", fleeing = "flee", "armed", "race", 'suspected mental illness' = "signs_of_mental_illness", 'body camera on' = "body_camera", "age" = 'age_bin', 'threat level' = "threat_level")
+
+#rename the race column for clarity 
+police_shootings$race[police_shootings$race == "W"] <- "White"
+police_shootings$race[police_shootings$race == "B"] <- "Black"
+police_shootings$race[police_shootings$race == "H"] <- "Hispanic"
+police_shootings$race[police_shootings$race == "N"] <- "Native American"
+police_shootings$race[police_shootings$race == "A"] <- "Asian"
+police_shootings$race[police_shootings$race == "O"] <- "Other"
+
+#put the ages into "Bins" 
+police_shootings$age_bin <- ifelse(police_shootings$age < 18, "Under 18",
+                       ifelse(police_shootings$age < 30, "18 - 30",
+                              ifelse(police_shootings$age < 40, "30 - 40",
+                                     ifelse(police_shootings$age < 50, "40 - 50", 
+                                            ifelse(police_shootings$age < 60, "50 - 60", "Over 60")))))
 #Sets up a UI Object
 ui <- fluidPage(
   
   titlePanel("Police Shootings Dashboard"),
-  sidebarPanel(
-    checkboxGroupInput(inputId = "states", label = "Choose State(s) to Compare", choices = sort(unique(police_shootings$state)), selected = c("WI", "IL"))
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      box(width = 12,
+          splitLayout(
+             checkboxGroupInput(inputId = "states", label = "State(s)", choices = sort(unique(police_shootings$state)), selected = c("WI", "IL")),
+                    radioButtons(inputId = "details", label = "Case Detail", choices = sort(possible_graphs) 
+          )
+      )
+    ),width= 3,
+      
   ),
+   position = "right",
+  
+  
   mainPanel(
     textOutput(outputId = "pie_label"),
     plotOutput(outputId = "piechart"),
@@ -15,21 +45,33 @@ ui <- fluidPage(
     tableOutput(outputId = "table")
   )
   
+  
+  )
+ 
+  
 )
+
+
 #Sets up a Server Object
 server <- function(input, output) {
   
   output$pie_label <- renderText("Comparison of Fatal Shootings by State")
   output$table_label <- renderText("Total Fatal Shootings of Selected States")
-  states <- reactive({
-    chosen_states <- filter(police_shootings, state %in% input$states)
-    states_table <- table(chosen_states$state)
+  chosen_states <- reactive({
+    chosen_states <- filter(police_shootings, state %in% input$states)})
+  
+  states_table <- reactive({
+    table(chosen_states()$state)
     })
   
+  chosen_data <- reactive({
+    this_data <- chosen_states()[,input$details]
+    table(this_data)
+  })
   
-  output$piechart <- renderPlot(pie(states()))
+  output$piechart <- renderPlot(pie(chosen_data()))
   #Show the counts and rename the Column names
-  output$table <- renderTable({df <- data.frame(states())
+  output$table <- renderTable({df <- data.frame(states_table())
                               df <- df %>% rename(State = Var1, Count = Freq)
     })
   
